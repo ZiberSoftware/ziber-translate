07a62903d54723c099c4ba33271ff3d6f4672b87
Kiliantessel <k.tessel@ziber.nl>
2014-03-20 10:24:32 +0100

    Cleaned securitycontroller and removed security views

--- ZiberTranslate.Web/Controllers/SecurityController.cs
+++ ZiberTranslate.Web/Controllers/SecurityController.cs
@@ -21,12 +21,7 @@ namespace ZiberTranslate.Web.Controllers
         public SecurityController(ISecurityService securityService)
         {
             this.securityService = securityService;
-        }
-
-        public ActionResult Login()
-        {
-            return View("Login");
-        }
+        }        
 
         [HttpPost]
         public ActionResult Login(string emailAddress, string password, string returnUrl = "")
@@ -47,58 +42,6 @@ namespace ZiberTranslate.Web.Controllers
             
             return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
-
-        //public ActionResult Signup()
-        //{
-        //    return View("Signup");
-        //}
-
-        //[HttpPost]
-        //public ActionResult Signup(string fullName, string emailSignup, string passwordSignup, string confirmPassword)
-        //{
-        //    var NUMBER_OF_ITERATIONS = 1337;
-        //    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-        //    byte[] buff = new byte [64];
-        //    rng.GetBytes(buff);
-        //    var salt = Convert.ToBase64String(buff);
-        //    var saltBytes = Encoding.UTF8.GetBytes(salt);
-        //    var pbkdf2 = new Rfc2898DeriveBytes(passwordSignup, saltBytes, NUMBER_OF_ITERATIONS);
-        //    var key = pbkdf2.GetBytes(128);
-        //    var hashKey = Convert.ToBase64String(key);
-        //    var checkUniqueMail = DbSession.QueryOver<Translator>()
-        //                        .Where(x => x.EmailAddress == emailSignup)
-        //                        .SingleOrDefault();
-
-        //    if (checkUniqueMail == null)
-        //    {
-        //        if (passwordSignup == confirmPassword)
-        //        {
-        //            using (var r = DbSession.BeginTransaction())
-        //            {
-        //                var newTranslator = new Translator();
-        //                newTranslator.Name = fullName;
-        //                newTranslator.EmailAddress = emailSignup;
-        //                newTranslator.Hash = hashKey;
-        //                newTranslator.Salt = salt;
-        //                newTranslator.Rank = 1;
-        //                newTranslator.IsBlocked = false;
-
-        //                Global.CurrentSession.Save(newTranslator);
-        //                r.Commit();
-        //            }
-
-        //            return View("Succes");
-        //        }
-        //        else
-        //        {
-        //            return Content("User input is invalid, please try again");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return Content("You allready signed up with this e-mailaddress");
-        //    }
-        //}
 
         public ActionResult Logout()
         { 
